6120c6eb0e03e6d6e78713c371f08dee527061a3
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-08-10 10:53:40 -0300

    feat: enhance MainPage layout with encoding/decoding features and input handling

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function MainPage() {
   return (
     <View style={styles.container}>
-      <Text>Main Page</Text>
+      <Text>Base 64</Text>
+      <View>
+        <TouchableOpacity>
+          <Text>Encode</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Decode</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput placeholder="Enter text here" />
+        <Text>0 Characters</Text>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Cut</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Copy</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Paste</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Clear</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>Consider space</Text>
+        <View />
+      </View>
+      <View>
+        <Text>Help the developer: </Text>
+        <TouchableOpacity>
+          <Text>Buy me a coffee</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,7 +54,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
