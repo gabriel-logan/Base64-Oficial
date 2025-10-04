aae988a3bb487d04a05853a0d2179be89b638de2
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-10-04 12:05:32 -0300

    feat: implement splash screen with loading indicator in App component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 import MainPage from "./src/pages/Main";
+import { useEffect, useState } from "react";
+
+SplashScreen.setOptions({
+  fade: true,
+});
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 700));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsLoading(false);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View
+        style={[
+          styles.container,
+          { justifyContent: "center", alignItems: "center" },
+        ]}
+      >
+        <ActivityIndicator size="large" color="#0084ff" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
